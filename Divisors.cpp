3f1c85e4e3b61f84fc915b41cf42e9f5a220e5ba
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2021-12-11 14:25:22 +0600

    Create 	Divisors.cpp

---
+++ Divisors.cpp
@@ -0,0 +1,32 @@
+Problem: https://toph.co/p/divisors
+
+#include<bits/stdc++.h>
+#define ff unsigned int ttt;cin>>ttt;for(int zzz=0;zzz<ttt;zzz++)
+#define ll long long int
+#define ui unsigned int
+#define ull unsigned long long int
+#define SPEED ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl '\n'
+#define Fi(ii,jj) for(int i=ii;i<jj;i++)
+#define Fj(iii,jjj) for(int j=iii;j<jjj;j++)
+
+using namespace std;
+void solve();
+
+
+
+main(){
+    SPEED  solve();
+    }
+
+
+
+
+void solve(){
+    int n;
+    cin>>n;
+
+    Fi(1,n+1)
+        if(n%i==0)
+            cout<<i<<nl;
+    }
