ae76a3ac78273e1e1cfb0c87e33ef730e6eb4ecb
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2021-12-11 14:42:23 +0600

    Create Is Anagram.cpp

---
+++ Is Anagram.cpp
@@ -0,0 +1,42 @@
+Link: https://toph.co/p/is-anagram
+
+#include<bits/stdc++.h>
+#define ff unsigned int ttt;cin>>ttt;for(int zzz=0;zzz<ttt;zzz++)
+#define ll long long int
+#define ui unsigned int
+#define ull unsigned long long int
+#define SPEED ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl '\n'
+#define Fi(ii,jj) for(int i=ii;i<jj;i++)
+#define Fj(iii,jjj) for(int j=iii;j<jjj;j++)
+#define strSort(sss) sort(sss.begin(),sss.end())
+
+using namespace std;
+void solve();
+
+
+
+main(){
+    SPEED  solve();
+    }
+
+
+
+
+void solve(){
+    string a, b;
+    cin>>a>>b;
+
+    strSort(a);
+    strSort(b);
+
+    Fi(0,a.size())
+        if(a[i]!=b[i]){
+            cout<<"No";
+            return;
+        }
+
+    cout<<"Yes";
+    return;
+
+    }
